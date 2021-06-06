ec18ba7a8b8caaa75b9eada5c3c94b2f571ad885
Elmeri Niemelä <niemela.elmeri@gmail.com>
2021-06-06 10:11:35 +0300

    Update pdfminer manifest

--- odoo_pdfminer/__manifest__.py
+++ odoo_pdfminer/__manifest__.py
@@ -1,32 +1,15 @@
 # -*- coding: utf-8 -*-
 {
     'name': "Odoo PDF Miner",
-
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
-
-    'description': """
-        Long description of module's purpose
-    """,
-
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
+    'author': "Elmeri Niemelä",
+    'website': "www.thecodebase.tech",
     'category': 'Uncategorized',
     'version': '0.1',
-
-    # any module necessary for this one to work correctly
     'depends': [
         'base',
         'account',
     ],
-    "external_dependencies": {"python": ["pdfminer"]},
-
-    # always loaded
+    "external_dependencies": {"python": ["pdfminer.six"]},
     'data': [
         'security/ir.model.access.csv',
         'wizards/account_import_pdfminer_view.xml',
