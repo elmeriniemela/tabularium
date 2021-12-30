1e877e38ba76255a52ab92c7bee94b2e79fa706b
Elmeri Niemelä <niemela.elmeri@gmail.com>
2021-12-30 09:18:23 +0200

    information_monitoring: Initial commit

---
+++ information_monitoring/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Information Monitoring",
+    'author': "Elmeri Niemelä",
+    'website': "https://eniemala.fi",
+    'category': 'Uncategorized',
+    'version': '0.1',
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/information_monitoring_views.xml',
+    ],
+}
