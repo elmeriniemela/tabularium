175c878c6ecd867db5358db01d1bc7009fa0a5ef
Elmeri Niemelä <niemela.elmeri@gmail.com>
2022-07-03 16:55:00 +0300

    Improve currency update

--- investment_portfolio/models/portfolio.py
+++ investment_portfolio/models/portfolio.py
@@ -11,31 +11,40 @@ _logger = logging.getLogger(__name__)
 class Currency(models.Model):
     _inherit = 'res.currency'
 
-    def cron_update_rate(self):
+    def cron_update_rate(self, mode='realtime'):
         Rate = self.env['res.currency.rate']
         Asset = self.env['investment.asset']
         currencies = {c.name: c for c in self.search([])}
         from_currency = self.env.company.currency_id.name
         api_key = self.env['ir.config_parameter'].sudo().get_param('alpha.vantage.api.key')
         for to_currency, currency_id in currencies.items():
-            resp = requests.get(f'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency={from_currency}&to_currency={to_currency}&apikey={api_key}')
 
-            vals = resp.json()["Realtime Currency Exchange Rate"]
 
-            date = dateutil.parser.parse(vals['6. Last Refreshed']).date()
-            rate = float(vals['5. Exchange Rate'])
-            rate_record = Rate.search([
-                ('currency_id', '=', currency_id.id),
-                ('name', '=', date),
-            ])
-            if rate_record:
-                rate_record.rate = rate
+            rates = {}
+            if mode == 'realtime':
+                resp = requests.get(f'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency={from_currency}&to_currency={to_currency}&apikey={api_key}')
+                vals = resp.json()["Realtime Currency Exchange Rate"]
+                rates = {dateutil.parser.parse(vals['6. Last Refreshed']).date(): float(vals['5. Exchange Rate'])}
+            elif mode == 'weekly':
+                resp = requests.get(f'https://www.alphavantage.co/query?function=FX_WEEKLY&from_symbol={from_currency}&to_symbol={to_currency}&apikey={api_key}')
+                vals = resp.json()["Time Series FX (Weekly)"]
+                rates = {dateutil.parser.parse(d_str).date(): float(w['4. close']) for d_str, w in vals.items() }
             else:
-                Rate.create({
-                    'name': date,
-                    'currency_id': currency_id.id,
-                    'rate': rate,
-                })
+                raise ValidationError(f"Invalid {mode=}")
+
+            for date, rate in rates.items():
+                rate_record = Rate.search([
+                    ('currency_id', '=', currency_id.id),
+                    ('name', '=', date),
+                ])
+                if rate_record:
+                    rate_record.rate = rate
+                else:
+                    Rate.create({
+                        'name': date,
+                        'currency_id': currency_id.id,
+                        'rate': rate,
+                    })
 
             Asset.search([('currency_id', '=', currency_id.id)])._compute_value()
 
