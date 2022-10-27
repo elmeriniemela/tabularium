eeca01b2ed0fdbec0634801853ddff5ddc169859
Elmeri Niemelä <niemela.elmeri@gmail.com>
2022-10-27 21:55:57 +0300

    Improve parent id and export id

--- toggl_sync/models/toggl_entry.py
+++ toggl_sync/models/toggl_entry.py
@@ -159,9 +159,9 @@ class TogglEntry(models.Model):
             if export_id and record.parent_id and not record.parent_id.export_id:
                 # Move to parent
                 record.export_id = False
-                record.flush('export_id')
+                record.flush(['export_id'])
                 record.parent_id.export_id = export_id
-                record.parent_id.export_id.flush('export_id')
+                record.parent_id.export_id.flush(['export_id'])
 
     @api.depends('task_id.task_id')
     def _compute_export_task_url(self):
