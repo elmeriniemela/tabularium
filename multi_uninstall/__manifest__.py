da9a4bfbde0524bee6aee9e2bffc52e5d6d5334b
Elmeri Niemelä <niemela.elmeri@gmail.com>
2022-11-05 15:29:55 +0200

    Add multi_uninstall

---
+++ multi_uninstall/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Multi Uninstall",
+    'author': "Elmeri Niemelä",
+    'website': "https://eniemela.fi",
+    'license': 'LGPL-3',
+    'category': 'Uncategorized',
+    'version': '0.1',
+    'depends': [
+        'base',
+    ],
+    'installable': True,
+    'application': False,
+    'data': [
+        'wizards/base_module_uninstall_views.xml'
+    ],
+}
---
+++ multi_uninstall/wizards/base_module_uninstall.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import api, fields, models, _
+
+
+class BaseModuleUninstall(models.TransientModel):
+    _inherit = "base.module.uninstall"
+
+
+    def action_next(self):
+        context = self.env.context.copy()
+        active = context.get('active_ids') or []
+        if len(active) > 1:
+            active.remove(self.module_id.id)
+            context['default_module_id'] = active[0]
+            return {
+                'name': _('Uninstall (%s left)') % len(active),
+                'view_mode': 'form',
+                'res_model': self._name,
+                'type': 'ir.actions.act_window',
+                'context': context,
+                'target': 'new',
+            }
+
+
+
+    def action_uninstall(self):
+        action = super().action_uninstall()
+        return self.action_next() or action
