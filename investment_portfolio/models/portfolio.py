29564fd8785937a82b7adcfa1fe992aa4846cf67
Elmeri Niemelä <niemela.elmeri@gmail.com>
2023-03-12 21:27:38 +0200

    Add price extrapolation

--- investment_portfolio/models/portfolio.py
+++ investment_portfolio/models/portfolio.py
@@ -891,6 +891,8 @@ class InvestmentAssetPrice(models.Model):
 
     prediction = fields.Boolean()
 
+    extrapolated = fields.Boolean()
+
     transaction_id = fields.Many2one(
         comodel_name='investment.asset.transaction',
         index=True,
@@ -899,6 +901,34 @@ class InvestmentAssetPrice(models.Model):
     _sql_constraints = [
         ('unique_price', 'unique(asset_id, time)', 'Price for this time is already configured!'),
     ]
+
+
+    def extrapolate_cagr(self):
+        assert len(self) == 2, "Two prices required"
+        Price = self.env['investment.asset.price']
+        first, last = self.sorted('time')
+        n = (last.time-first.time).days/365
+        cagr = ((last.price/first.price)**(1/n) - 1)
+
+        time = first.time
+        price = first.price
+        while time < (last.time + relativedelta(months=1)):
+            time += relativedelta(months=1)
+            price *= (1+cagr)**(1/12)
+            base_vals = {
+                'extrapolated': True,
+                'asset_id': first.asset_id.id,
+                'time': time,
+            }
+            price_id = Price.search([(key, '=', value) for key, value in base_vals.items()])
+            if not price_id:
+                price_id = Price.create({**base_vals, **{'price': price}})
+            else:
+                price_id.price = price
+
+
+
+
 
 
 class InvestmentAssetTransaction(models.Model):
