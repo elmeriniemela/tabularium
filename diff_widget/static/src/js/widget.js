c13710f3995235d5367343dab680cea1c293b03e
Elmeri Niemelä <niemela.elmeri@gmail.com>
2024-01-03 10:30:57 +0200

    add diff field

---
+++ diff_widget/static/src/js/widget.js
@@ -0,0 +1,30 @@
+/** @odoo-module **/
+
+import { _lt } from "@web/core/l10n/translation";
+import { registry } from "@web/core/registry";
+import { Component, onWillStart, markup } from "@odoo/owl";
+
+export class DiffField extends Component {
+
+    setup() {
+    }
+
+    get formattedValue() {
+        var configuration = {
+            drawFileList: true,
+            fileListToggle: false,
+            fileListStartVisible: false,
+            fileContentToggle: false,
+            matching: 'lines',
+            outputFormat: 'side-by-side',
+            synchronisedScroll: true,
+            highlight: true,
+            renderNothingWhenEmpty: false,
+        };
+        var diffHtml = Diff2Html.html(this.props.value, configuration);
+        return markup(diffHtml);
+    }
+}
+DiffField.template = "diff_widget.DiffField";
+DiffField.displayName = _lt("Diff");
+registry.category("fields").add("diff", DiffField);
