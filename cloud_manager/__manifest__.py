ff03f7e6767b6c4bce554f8bbf0efb5008d0faa5
Elmeri Niemelä <niemela.elmeri@gmail.com>
2024-01-29 19:33:56 +0200

    Fix action name

--- cloud_manager/__manifest__.py
+++ cloud_manager/__manifest__.py
@@ -6,7 +6,7 @@
     'website': "https://eniemela.fi",
     'license': 'LGPL-3',
     'category': 'Uncategorized',
-    'version': '1.0.3',
+    'version': '1.0.4',
     'depends': [
         'base',
         'mail',
