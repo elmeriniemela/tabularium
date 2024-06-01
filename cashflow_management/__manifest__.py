be3df30694aa1196bb8080079c28f50874d5850b
Elmeri Niemelä <niemela.elmeri@gmail.com>
2024-06-01 11:30:13 +0300

    Add reporting views

--- cashflow_management/__manifest__.py
+++ cashflow_management/__manifest__.py
@@ -5,7 +5,7 @@
     'website': "https://eniemela.fi",
     'license': 'LGPL-3',
     'category': 'Cash Flow',
-    'version': '0.1.1',
+    'version': '1.0.0',
     'depends': [
         'mail',
     ],
