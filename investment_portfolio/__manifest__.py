9dfc6d6f0c86b4864a04b32abd707e2470394276
Elmeri Niemelä <niemela.elmeri@gmail.com>
2024-06-07 16:02:24 +0300

    Improvements

--- investment_portfolio/__manifest__.py
+++ investment_portfolio/__manifest__.py
@@ -5,7 +5,7 @@
     'website': "https://eniemela.fi",
     'license': 'LGPL-3',
     'category': 'Investment Portfolio',
-    'version': '2.2.5',
+    'version': '2.2.6',
     'depends': [
         'mail',
         'timeago_widget',
