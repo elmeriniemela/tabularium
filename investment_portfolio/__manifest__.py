438fbf4049ea80cc7402ad421e149b822ba4bfd8
Elmeri Niemelä <niemela.elmeri@gmail.com>
2024-06-07 19:51:34 +0300

    view timeseries

--- investment_portfolio/__manifest__.py
+++ investment_portfolio/__manifest__.py
@@ -5,7 +5,7 @@
     'website': "https://eniemela.fi",
     'license': 'LGPL-3',
     'category': 'Investment Portfolio',
-    'version': '2.2.7',
+    'version': '2.2.8',
     'depends': [
         'mail',
         'timeago_widget',
