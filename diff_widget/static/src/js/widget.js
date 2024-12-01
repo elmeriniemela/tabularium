de84b3d84c0a811e9bee3cab91a4134915d3c3bb
Elmeri Niemelä <niemela.elmeri@gmail.com>
2024-12-01 19:42:15 +0200

    migrate to 17.0

--- diff_widget/static/src/js/widget.js
+++ diff_widget/static/src/js/widget.js
@@ -1,21 +1,24 @@
 /** @odoo-module **/
 
-import { _lt } from "@web/core/l10n/translation";
+import { _t } from "@web/core/l10n/translation";
 import { registry } from "@web/core/registry";
 import { Component, markup } from "@odoo/owl";
 import { standardFieldProps } from "@web/views/fields/standard_field_props";
 
 export class DiffField extends Component {
-
-    setup() {
-    }
+    static template = "diff_widget.DiffField";
+    static props = {
+        ...standardFieldProps,
+        maxLength: { type: Number, optional: true },
+    };
 
     get formattedValue() {
-        if (!this.props.value) {
+        var value = this.props.record.data[this.props.name];
+        if (!value) {
             return '';
         }
-        if (this.props.maxLength && this.props.value.length > this.props.maxLength) {
-            return markup(`<p>diff: maxLength ${this.props.maxLength} exceeded: ${this.props.value.length}</p>`);
+        if (this.props.maxLength && value.length > this.props.maxLength) {
+            return markup(`<p>diff: maxLength ${this.props.maxLength} exceeded: ${value.length}</p>`);
         }
         var configuration = {
             drawFileList: true,
@@ -31,21 +34,19 @@ export class DiffField extends Component {
             diffMaxLineLength: 500,
             diffTooBigMessage: "Diff is too big to show.",
         };
-        var diffHtml = Diff2Html.html(this.props.value, configuration);
+        var diffHtml = Diff2Html.html(value, configuration);
         return markup(diffHtml);
     }
 }
-DiffField.template = "diff_widget.DiffField";
-DiffField.displayName = _lt("Diff");
-DiffField.supportedTypes = ["text"];
-DiffField.props = {
-    ...standardFieldProps,
-    maxLength: { type: Number, optional: true },
+
+export const diffField = {
+    component: DiffField,
+    displayName: _t("Diff"),
+    supportedTypes: ["text"],
+    extractProps: ({ options }) => {
+        return { maxLength: options.maxLength };
+    },
 };
 
-DiffField.extractProps = ({ attrs }) => {
-    return {
-        maxLength: attrs.options.maxLength,
-    };
-};
-registry.category("fields").add("diff", DiffField);
+
+registry.category("fields").add("diff", diffField);
