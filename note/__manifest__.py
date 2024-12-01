c51b158682f320c371a21c73ac83001982b4be59
Elmeri Niemelä <niemela.elmeri@gmail.com>
2024-12-01 18:16:28 +0200

    add notes

---
+++ note/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Notes',
+    'version': '1.0',
+    'category': 'Productivity/Notes',
+    'summary': 'Organize your work with memos',
+    'sequence': 260,
+    'depends': [
+        'mail',
+    ],
+    'data': [
+        'security/note_security.xml',
+        'security/ir.model.access.csv',
+        'data/mail_activity_type_data.xml',
+        'data/note_data.xml',
+        'data/res_users_data.xml',
+        'views/note_views.xml',
+        ],
+    'demo': [
+        'data/note_demo.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
