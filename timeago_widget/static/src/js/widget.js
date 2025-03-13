78adfaf6b13bdd6a5952f241a15c3a3542187ba4
Elmeri Niemelä <niemela.elmeri@gmail.com>
2025-03-13 18:13:40 +0200

    shorter timeago

--- timeago_widget/static/src/js/widget.js
+++ timeago_widget/static/src/js/widget.js
@@ -3,8 +3,33 @@
 import { _t } from "@web/core/l10n/translation";
 import { registry } from "@web/core/registry";
 import { Component } from "@odoo/owl";
+import { standardFieldProps } from "@web/views/fields/standard_field_props";
+
 export class TimeagoField extends Component {
     static template = "timeago_widget.TimeagoField";
+    static props = { ...standardFieldProps };
+
+    setup() {
+        jQuery.timeago.settings.strings = {
+            prefixAgo: null,
+            prefixFromNow: null,
+            suffixAgo: "",
+            suffixFromNow: "",
+            seconds: "1m",
+            minute: "1m",
+            minutes: "%dm",
+            hour: "1h",
+            hours: "%dh",
+            day: "1d",
+            days: "%dd",
+            month: "1mo",
+            months: "%dmo",
+            year: "1yr",
+            years: "%dyr",
+            wordSeparator: " ",
+            numbers: []
+        };
+    }
 
     get isoValue() {
         var date = new Date(this.props.record.data[this.props.name]);
