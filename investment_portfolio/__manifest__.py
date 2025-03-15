0ebdc72f9425dc26ddf850c656c488a7241778e4
Elmeri Niemelä <niemela.elmeri@gmail.com>
2025-03-15 12:37:13 +0200

    add liquid filter

--- investment_portfolio/__manifest__.py
+++ investment_portfolio/__manifest__.py
@@ -5,7 +5,7 @@
     'website': "https://eniemela.fi",
     'license': 'LGPL-3',
     'category': 'Investment Portfolio',
-    'version': '2.5.6',
+    'version': '2.5.7',
     'depends': [
         'mail',
         'timeago_widget',
