58d96be2db4bf20f1fb171bfc3a5a9a5d78b5a87
Elmeri Niemelä <niemela.elmeri@gmail.com>
2025-04-20 20:52:12 +0300

    add toggl

--- toggl_sync/wizards/toggl_import.py
+++ toggl_sync/wizards/toggl_import.py
@@ -16,7 +16,7 @@ class TogglImport(models.TransientModel):
 
     start_date = fields.Datetime(
         required=True,
-        default=lambda self: self.env['toggl.entry'].search([('export_id', '!=', False)], limit=1).date_stop - datetime.timedelta(days=1)
+        default=lambda self: (self.env['toggl.entry'].search([('export_id', '!=', False)], limit=1).date_stop or datetime.datetime.now()) - datetime.timedelta(days=1)
     )
 
     end_date = fields.Datetime(
