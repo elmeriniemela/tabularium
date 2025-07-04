5098d5446288fa4b3c622b0d8e49df931b05215b
Elmeri Niemelä <niemela.elmeri@gmail.com>
2025-07-04 11:27:43 +0300

    fix widget

--- note/__manifest__.py
+++ note/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Notes',
-    'version': '1.2',
+    'version': '1.3',
     'author': "Elmeri Niemelä",
     'category': 'Productivity/Notes',
     'summary': 'Organize your work with memos',
