90808888a5749efb5e4c9ece461a83116f7de9d2
Elmeri Niemelä <niemela.elmeri@gmail.com>
2025-07-05 17:56:11 +0300

    version bump

--- cloud_manager/__manifest__.py
+++ cloud_manager/__manifest__.py
@@ -6,7 +6,7 @@
     'website': "https://eniemela.fi",
     'license': 'LGPL-3',
     'category': 'Uncategorized',
-    'version': '1.2.5',
+    'version': '1.2.6',
     'depends': [
         'base',
         'mail',
