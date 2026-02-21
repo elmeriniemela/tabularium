30d034323d69896663990260e7dcae78e76ada91
Elmeri Niemelä <niemela.elmeri@gmail.com>
2026-02-21 13:34:03 +0200

    add external ref search

--- investment_portfolio/__manifest__.py
+++ investment_portfolio/__manifest__.py
@@ -5,7 +5,7 @@
     'website': "https://eniemela.fi",
     'license': 'LGPL-3',
     'category': 'Investment Portfolio',
-    'version': '2.8.19',
+    'version': '2.8.20',
     'depends': [
         'mail',
         'timeago_widget',
