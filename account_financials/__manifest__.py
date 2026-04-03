0d97ef83b39cf8941c48707c728fd950ce010575
Elmeri Niemelä <niemela.elmeri@gmail.com>
2026-04-03 10:45:08 +0300

    bump version

--- account_financials/__manifest__.py
+++ account_financials/__manifest__.py
@@ -5,7 +5,7 @@
     'website': 'https://eniemela.fi',
     'license': 'LGPL-3',
     'category': 'Accounting',
-    'version': '0.1.3',
+    'version': '1.0.0',
     'depends': [
         'account',
     ],
