a34c96c868f13a333cab70e72e22fa5c62824d8d
Elmeri Niemelä <niemela.elmeri@gmail.com>
2026-04-04 11:25:46 +0300

    fix icons

--- web_theme_mods/controllers/webmanifest.py
+++ web_theme_mods/controllers/webmanifest.py
@@ -2,14 +2,16 @@ from odoo.addons.web.controllers import webmanifest
 
 
 class WebManifest(webmanifest.WebManifest):
+    "Overwrite _get_webmanifest"
 
     def _get_webmanifest(self):
         manifest = super()._get_webmanifest()
+        icon_sizes = ['192x192', '512x512']
         manifest['icons'] = [{
-            'src': '/web_theme_mods/static/description/icon.png',
-            'sizes': 'any',
+            'src': '/web_theme_mods/static/img/odoo-icon-%s.png' % size,
+            'sizes': size,
             'type': 'image/png',
-        }]
+        } for size in icon_sizes]
         return manifest
 
 
