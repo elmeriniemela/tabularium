6dc870a21012d47bfa482d68abea7c67ce893a01
Elmeri Niemelä <niemela.elmeri@gmail.com>
2026-04-04 09:53:26 +0300

    rename web_theme_mods

---
+++ web_theme_mods/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Backend Theme MODS',
+    'version': '1.0',
+    'category': 'Themes/Backend',
+    'license': 'LGPL-3',
+    'author': 'Elmeri Niemelä',
+    'description': "1. Add a custom favicon to backend.",
+    'depends': [
+        'web',
+    ],
+    'data': [
+
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
