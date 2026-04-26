c8bba3adf12128a4e1f365a222f74aff075c9a59
Elmeri Niemelä <niemela.elmeri@gmail.com>
2026-04-26 12:12:14 +0300

    Revert "hide decimals if prices greater than 1000"
    This reverts commit 535e04bf4eeea1f3602b9b86b14a92c492e30bca.

--- chart_widget/static/src/chart_renderer.js
+++ chart_widget/static/src/chart_renderer.js
@@ -3,17 +3,6 @@
 import { loadBundle } from "@web/core/assets";
 import { Component, onWillStart, useEffect, useRef, onWillUnmount } from "@odoo/owl";
 
-const DECIMAL_HIDING_INTERVAL = 1000;
-const INTEGER_PRICE_FORMAT = {
-    type: "price",
-    precision: 0,
-    minMove: 1,
-};
-
-export function shouldHidePriceDecimals(visibleRange) {
-    return !!visibleRange && Math.abs(visibleRange.to - visibleRange.from) > DECIMAL_HIDING_INTERVAL;
-}
-
 export class ChartRenderer extends Component {
     static template = "chart_widget.ChartRenderer";
     static props = {
@@ -23,8 +12,6 @@ export class ChartRenderer extends Component {
     setup() {
         this.containerRef = useRef("container");
         this.chart = null;
-        this.renderedSeries = [];
-        this.visibleLogicalRangeChangeHandler = null;
 
         onWillStart(async () => {
             await loadBundle("chart_widget.lightweight_charts");
@@ -45,16 +32,9 @@ export class ChartRenderer extends Component {
 
     _destroyChart() {
         if (this.chart) {
-            if (this.visibleLogicalRangeChangeHandler) {
-                this.chart
-                    .timeScale()
-                    .unsubscribeVisibleLogicalRangeChange(this.visibleLogicalRangeChangeHandler);
-                this.visibleLogicalRangeChangeHandler = null;
-            }
             this.chart.remove();
             this.chart = null;
         }
-        this.renderedSeries = [];
     }
 
     _renderChart() {
@@ -88,34 +68,8 @@ export class ChartRenderer extends Component {
             const definition = LWC[`${seriesConfig.type}Series`] || LWC.LineSeries;
             const series = this.chart.addSeries(definition, options);
             series.setData(seriesConfig.data);
-            this.renderedSeries.push({
-                api: series,
-                defaultPriceFormat: { ...series.options().priceFormat },
-                hidesDecimals: null,
-            });
         }
 
-        const timeScale = this.chart.timeScale();
-        this.visibleLogicalRangeChangeHandler = () => this._updatePriceFormats();
-        timeScale.subscribeVisibleLogicalRangeChange(this.visibleLogicalRangeChangeHandler);
-        timeScale.fitContent();
-        this._updatePriceFormats();
-    }
-
-    _updatePriceFormats() {
-        for (const renderedSeries of this.renderedSeries) {
-            const hidesDecimals = shouldHidePriceDecimals(
-                renderedSeries.api.priceScale().getVisibleRange()
-            );
-            if (renderedSeries.hidesDecimals === hidesDecimals) {
-                continue;
-            }
-            renderedSeries.api.applyOptions({
-                priceFormat: hidesDecimals
-                    ? INTEGER_PRICE_FORMAT
-                    : renderedSeries.defaultPriceFormat,
-            });
-            renderedSeries.hidesDecimals = hidesDecimals;
-        }
+        this.chart.timeScale().fitContent();
     }
 }
